27d953c50fb31f9624bb1da9b278f8ed66fcf1cb
Shakha Suman <shakhasuman@62-0e-bf-da-b3-84-dyn.staff.utas.edu.au>
2025-08-07 16:19:47 +1000

    Updated all new files

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    console.log("Document is ready ");
+    // $("#header").load("header.html",
+    //     function () {
+
+    //         console.log("Header loaded ");
+
+    //         // Remove 'active' class from all .nav-link elements
+
+    //         $(".nav-link").removeClass("active");
+    //         // Set up click handlers
+
+    //         console.log("Setting up click handlers ");
+
+    //         $(".nav-link").each(function () {
+
+    //             if (this.href ==
+    //                 window.location.href) {
+
+    //                 $(this).addClass("active");
+
+    //             }
+
+    //         });
+
+    //         console.log("Click handlers set up ");
+
+    //     });
+
+    // $("#footer").load("footer.html",
+    //     function () {
+
+    //         console.log("Footer loaded ");
+
+    //     });
+
+});
